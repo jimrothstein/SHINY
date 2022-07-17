1dc132675a809f00d4580faba991677626e95825
jimrothstein <jimrothstein@gmail.com>
2022-07-16 22:38:36 -0700

    first

---
+++ 100_first_shiny.R
@@ -0,0 +1,36 @@
+library(shiny)
+
+
+#			PURPOSE:		First shiny app
+#
+#			USAGE:			(source(<file>))    (Note:  need  outer paranthesis, like printing)
+#
+#									Also can open 2nd, exteranl browwer to see same results.
+#									Reactive:   code is 'recipe' to fire under right
+#									conditions.
+#
+ui <- fluidPage(
+  "Hello, world!"
+)
+
+#  next
+ui <- fluidPage(
+  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table")
+)
+
+server <- function(input, output, session) {
+}
+server <- function(input, output, session) {
+  output$summary <- renderPrint({
+    dataset <- get(input$dataset, "package:datasets")
+    summary(dataset)
+  })
+  
+  output$table <- renderTable({
+    dataset <- get(input$dataset, "package:datasets")
+    dataset
+  })
+}
+shinyApp(ui, server)
