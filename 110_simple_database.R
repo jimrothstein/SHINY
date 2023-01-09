1d8a9b4d5f5dedbb5e0917104f0f3eb487385cb7
jimrothstein <jimrothstein@gmail.com>
2023-01-09 01:22:12 -0800

    wip

---
+++ 110_simple_database.R
@@ -0,0 +1,17 @@
+library(shiny)
+### Only run this example in interactive R sessions
+if (interactive()) {
+  # table example
+  shinyApp(
+    ui = fluidPage(
+      fluidRow(
+        column(12,
+          tableOutput('table')
+        )
+      )
+    ),
+    server = function(input, output) {
+      output$table <- renderTable(mtcars)
+    }
+  )
+}
