21428fb41b8123bd49fe31d2c3f14f733e3ffd47
jimrothstein <jimrothstein@gmail.com>
2023-02-13 15:08:39 -0800

    Ch4 - polish - wip

--- chapter04/20_polish_tables.R
+++ chapter04/20_polish_tables.R
@@ -3,19 +3,43 @@
 # REF:    Mastering Shiny, Chapter 04.5, ER injuries
 # REF:    https://www.cpsc.gov/Research--Statistics/NEISS-Injury-Data
 #        cribs, shopping cart .... injuries
-#
+
+# PURPOSE:  Polish tables, using forcats::
 # USAGE:  (source("10_prototype.R"))
 # --------------------------------------------------------------------
 #
+# PROBLEM:   for diag(nosis) find top 5 and freq.
 library(shiny)
 library(vroom)
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(forcats)
 
 ##  tries to install `BH`, which has errors (don't want to waste  time on this
 ##  now) -- see issues in httpgd
 if (F) library(httpgd) # control plots easily 
+
+
+# ---------------------------------------  options() ---------------------------------------
+# Goal:   fix digits displayed
+
+# few options for printing format
+options() |> length()         #92  (of list)
+options() |> names()
+options() |> names() |> grep(pattern="dig*")
+options()[4]
+
+# sprintf is better, also prettNum()
+sprintf("%e", pi)
+sprintf("%e", pi/10)
+
+
+# for NOW, just divide by 1000 (cases per 1000 people)
+
+# ------------------------------------------------------------------------------
+
+
 
 
 # change "master" to "main", then works 
@@ -50,7 +74,27 @@ products
 population <- vroom::vroom("../neiss/population.tsv")
 population
 
+nrow(injuries)  
 
+# ------------------------------------------------------------------------------
+##  documenation:  use fct_lump_n, not fct_lump,  
+##  item 6 is all all "other"
+##  fct_* must convert diag frm char to factor   (I like data.table!)
+injuries %>%
+  mutate(diag = fct_lump_n(fct_infreq(diag), n = 5)) %>%      ##  `in order of freq`, 
+  group_by(diag) %>%
+  summarise(n = as.integer(sum(weight)/1000))           # /1000 cases per 1000 people
+
+library(data.table)
+dt  <- as.data.table(injuries)
+dt
+x  <- dt[, list(diag,sex)]
+x
+dt[, by=diag, .N][
+
+
+
+# ---------------------------- LEGACY ------------------------------
 # numeric[]
 prod_codes <- setNames(products$prod_code, products$title)
 
