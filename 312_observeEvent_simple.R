2fd323a08ef52130b59db899562f8502534fd1cd
jimrothstein <jimrothstein@gmail.com>
2025-01-09 21:27:29 -0800

    add observeEvent

---
+++ 312_observeEvent_simple.R
@@ -0,0 +1,17 @@
+# observeEvent
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
+)
+
+server <- function(input, output, session) {
+  string <- reactive(paste0("Hello ", input$name, "!"))
+  
+  output$greeting <- renderText(string())
+  observeEvent(input$name, {
+    message("Greeting performed (handler for observeEvent)", as.character(date()))
+  })
+}
+
+shinyApp(ui, server)
