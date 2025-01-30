1905d0078997304345c3e8f65b171b192f285348
jimrothstein <jimrothstein@gmail.com>
2025-01-29 20:29:38 -0800

    wip

--- shinylive/do_first.R
+++ shinylive/do_first.R
@@ -1,19 +1,21 @@
 ## do_first.R
 
-setwd("~/code/SHINY/shinylive")
+
+
+setwd("~/code/shiny_project/shinylive")
 dir()
 
-# Copy an existing app, to directory myapp
+# Copy an existing app, to directory shinylive/myapp
 
-# Copy "Hello World" from `{shiny}`
+# Example: Copy "Hello World" from `{shiny}`
 
 # find file 
 path = system.file("examples", "01_hello", package="shiny") 
 
-# copy it
-fs::dir_copy(path, "./shinylive/myapp", overwrite = TRUE)
+# copy it  (~/code/shinylive/myapp) creates `myapp` for you.
+fs::dir_copy(path, "./myapp", overwrite = TRUE)
 
-# `convert` and download web `assets` - wait!
+# `convert` and download web `assets` - wait!  (both myapp, site under shinylive)
 shinylive::export("myapp", "site")
 
 
@@ -21,5 +23,6 @@ shinylive::export("myapp", "site")
 httpuv::runStaticServer("site")
 
 
-# workflow
+
+# workflow - does what?
 usethis::use_github_action(url="https://github.com/posit-dev/r-shinylive/blob/actions-v1/examples/deploy-app.yaml")
