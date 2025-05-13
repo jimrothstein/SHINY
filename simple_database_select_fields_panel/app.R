15cd86c060ef34ea89da0ebc109ca9c8a24eb1b3
jimrothstein <jimrothstein@gmail.com>
2025-05-12 20:51:19 -0700

    wip

---
+++ simple_database_select_fields_panel/app.R
@@ -0,0 +1,64 @@
+
+# in 1 file, combine ui.R, server.R and run as app.
+library(shiny)
+library(ggplot2)
+
+# sidebar Panel, selectInput, mainPanel .... select from dataset
+dataset <- diamonds
+
+ui  <- shiny::fluidPage(
+
+  titlePanel("Diamonds Explorer"),
+
+  sidebarPanel(
+
+    # insist only integer sample sizes?
+    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
+                value=min(1000, nrow(dataset)), step=500, round=0),
+
+    selectInput('x', 'X', names(dataset)),
+    selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
+    selectInput('color', 'Color', c('None', names(dataset))),
+
+    checkboxInput('jitter', 'Jitter'),
+    checkboxInput('smooth', 'Smooth'),
+
+    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
+  ),
+
+  mainPanel(
+    plotOutput('plot')
+  )
+)
+
+
+server  <- function(input, output) {
+
+  dataset <- reactive({
+    diamonds[sample(nrow(diamonds), input$sampleSize),]
+  })
+
+  output$plot <- renderPlot({
+
+    p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_point()
+
+    if (input$color != 'None')
+      p <- p + aes_string(color=input$color)
+
+    facets <- paste(input$facet_row, '~', input$facet_col)
+    if (facets != '. ~ .')
+      p <- p + facet_grid(facets)
+
+    if (input$jitter)
+      p <- p + geom_jitter()
+    if (input$smooth)
+      p <- p + geom_smooth()
+
+    print(p)
+
+  }, height=700)
+
+}
+
+shinyApp(ui, server)
